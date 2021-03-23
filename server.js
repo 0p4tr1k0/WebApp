b840e7df54afe89a30177fa2133d4ce2bbcc3a6e
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2021-03-23 08:28:24 +0100

    Základ chatu

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const paht = require('path')
+const { report } = require('process')
 
 const app = express()
 const http = require('http').createServer(app)
 
+app.use(express.static(path.join(__dirname, 'public')))
 const PORT = process.env.PORT || 3000
 http.listen(PORT, () =>{
     console.log('Server běží na portu', PORT)
