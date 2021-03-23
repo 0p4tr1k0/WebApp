7570df6f333b43c7d00e2b9365fb8a0efb938b2e
0p4tr1k0 <BeruJenHotovostZRukyDoRuky@seznam.cz>
2021-03-23 11:26:21 +0100

    propojování socket io a serveru

--- public/aplikace.js
+++ public/aplikace.js
@@ -2,7 +2,7 @@ const socket = io();
 const msgText = document.querySelector('msg')
 const btnSend = document.querySelector('#btn-send')
 const chatBox = document.querySelector('.chat-content')
-const msgText = document.querySelector('.message')
+const displayMsg = document.querySelector('.message')
 
 let name;
 do{
@@ -12,6 +12,42 @@ do{
 document.querySelector('#your-name').textContent = name
 msgText.focus()
 
-btnSend.addEventListener('click', ()=>{
-    alert(123)
+btnSend.addEventListener('click', (e)=>{
+    e.preventDefault()
+    sendMsg(msgText.value)
+    msgText.value = '';
+    msgText.focus()
 })
+
+const sendMsg = message =>{
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    display(msg, 'you-message')
+    socket.emit('sendMessage', msg)
+}
+socket.on('sendToAll', msg=>{
+    displayMsg(msg, 'other-message')
+})
+
+const display = (msg, type)=>{
+    const msgDiv = document.createElement('div')
+    let className = type
+    msgDiv.classList.add(classname, 'message-row')
+    let times = new Date().toLocateTimeString()
+
+    let innerText = `
+                            <div class="message-title">
+                                <span>${msg.user}</span>
+                            </div>
+                            <div class="message-text">
+                                ${msg.message}
+                            </div>
+                            <div class="message-time">
+                                ${times}
+                            </div>
+    `;
+    msgDiv.innerHTML = innerText;
+    displayMsg.appendChild(msgDiv)
+}
